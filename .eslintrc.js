6e04cdbe199ef7b57d67ee6c57748df852964dd0
gebixiaowang <gebixiaowang@foxmail.com>
2020-09-03 10:56:17 +0800

    导入了vant组件库

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,27 @@
 module.exports = {
-  root: true,
+  root: false,
+
   env: {
-    node: true
+    node: false
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'space-before-function-paren': 0
+  },
+
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ]
 }
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <i class="iconfont icon-yanzhengma"></i>
+    <i class="iconfont icon-yuedu"></i>
+    <i class="iconfont icon-shipin"></i>
+    <i class="iconfont icon-dianzan"></i>
+    <van-button round type="info">圆形按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
+    <!-- 路由占位符 -->
+    <router-view />
   </div>
 </template>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: 'App',
+  methods: {
+    get() {
+      console.log(1)
     }
   }
+}
+</script>
+<style lang="less">
+.iconfont {
+  font-size: 80px !important;
+  margin: 20px;
+  font-weight: 500;
+  color: #fff;
 }
 </style>
